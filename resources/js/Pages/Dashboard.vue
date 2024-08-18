68a743dfd0e69d82c7df7c782d12239fd7ea99aa
kollence <misterija@gmail.com>
2024-08-18 15:22:46 +0200

    AppLayout as parent to all page components

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,11 +1,9 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
-        <template #header>
+        <template>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Dashboard
             </h2>
@@ -18,5 +16,4 @@ import Welcome from '@/Components/Welcome.vue';
                 </div>
             </div>
         </div>
-    </AppLayout>
 </template>
