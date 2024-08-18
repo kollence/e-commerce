1e5e60c40ef61680292092e583df9984ad5c67dc
kollence <misterija@gmail.com>
2024-08-18 14:45:08 +0200

    Jetstream installation with Inertia & vue

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,24 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-
-Route::resource('categories', App\Http\Controllers\CategoryController::class);
-
-Route::resource('brands', App\Http\Controllers\BrandController::class);
-
-Route::resource('products', App\Http\Controllers\ProductController::class);
-
-Route::resource('colors', App\Http\Controllers\ColorController::class);
-
-Route::resource('size-categories', App\Http\Controllers\SizeCategoryController::class);
-
-Route::resource('size-options', App\Http\Controllers\SizeOptionController::class);
-
-Route::resource('orders', App\Http\Controllers\OrderController::class);
-
-Route::resource('users', App\Http\Controllers\UserController::class);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
