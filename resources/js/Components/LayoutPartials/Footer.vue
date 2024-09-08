5743f4b1c016f890c2464c129010fa2a470f5937
kollence <misterija@gmail.com>
2024-09-09 01:21:29 +0200

    Footer and Layout header got animated gradient

--- resources/js/Components/LayoutPartials/Footer.vue
+++ resources/js/Components/LayoutPartials/Footer.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-    <footer class="text-center text-white bg-black px-4 py-4">
-        <div>
+    <footer class="text-center text-white bg-black px-4 py-4 shadow bg-gradient-to-r from-emerald-300 via-teal-600 to-cyan-950 animate-gradient-xy">
+        <div class="">
             <p>Layout footer </p>
             <p>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum ratione tenetur totam, nostrum
