7c956c205cb4cc511f9ad42a43369f3ef014e0c4
kollence <misterija@gmail.com>
2024-09-09 21:33:21 +0200

    Initial first size option is loaded, with every selected size option goes info from pivot

--- resources/js/Pages/Shop/Show.vue
+++ resources/js/Pages/Shop/Show.vue
@@ -2,13 +2,19 @@
 import NavigationHeader from '@/Components/NavigationHeader.vue';
 import NavCategories from '@/Components/Shop/NavCategories.vue';
 import { Link, Head } from '@inertiajs/vue3';
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 
-defineProps({
+const props = defineProps({
     product: Object,
 });
+const selectedSizeOptionId = ref(props.product.lowest_priced_item.size_options[0].id);
+const selectedSizeOption = ref(props.product.lowest_priced_item.size_options[0]);
 
-
+function updateSelectedSizeOption() {
+    selectedSizeOption.value = props.product.lowest_priced_item.size_options.find(
+    size_option => size_option.id === parseInt(selectedSizeOptionId.value)
+    );
+}
 
 </script>
 
@@ -32,18 +38,61 @@ defineProps({
         <div class="container text-stone-100 mx-auto px-4 py-8 justify-end">
             <div class="grid grid-cols-1 md:grid-cols-12 gap-2 mb-5">
                 <div class="md:col-span-2">
+                    <!-- <small>Product item images</small> -->
                     <img v-for="(image, i) in product.lowest_priced_item.images" :key="i" :src="image.url" :alt="product.name"/> 
                 </div>
                 <div class="md:col-span-6 ">
-                    <img :src="product.images[0].url" :alt="product.name"
-                        class="object-cover border border-gray-700 rounded-lg">
+                    <img :src="product.images[0].url" :alt="product.name" class="object-cover border border-gray-700 rounded-lg">
                 </div>
                 <div class="md:col-span-4">
                     <h1 class="text-3xl font-bold mb-4">{{ product.name }}</h1>
-                    <div class="flex items-end mb-4 border-b border-gray-300 pb-2">
+                    <div class="flex items-end mb-4  pb-2">
 
                         <span class="  mr-2">Brand:</span>
                         <p class="text-lg">{{ product.brand.name }}</p>
+                    </div>
+                    <div class="flex justify-start items-center mb-1 pb-1 border-b border-gray-300">
+                        <div class="flex items-center mb-1  pb-1">
+                            <span class=" mr-2">Is Featured:</span>
+                            <span :class="product.is_featured ? 'text-green-100' : 'text-red-600'">{{
+                                product.is_featured ?
+                                    '&#x2714;'
+                                : ' &#x2716;' }}</span>
+                        </div>
+                        <div class="flex items-center mb-1  pb-1 ml-5">
+                            <span class=" mr-2">Product code:</span>
+                            <span>{{product.lowest_priced_item.product_code}}</span>
+                        </div>
+                    </div>
+
+                    <div >
+                        <label for="size_options" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Select size option
+                        </label>
+                        <select id="size_options" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            v-model="selectedSizeOptionId"
+                            @change="updateSelectedSizeOption"    
+                        >
+                            <option v-for="size_options in product.lowest_priced_item.size_options" :key="size_options.id" :value="size_options.id">{{ size_options.name }}</option>
+                        </select>
+                    </div>
+                    <div v-if="selectedSizeOption" class="ml-1 mt-1">
+                        <p><strong>SKU:</strong> {{ selectedSizeOption.pivot.sku }}</p>
+                        <p><strong>In Stock:</strong> {{ selectedSizeOption.pivot.in_stock }}</p>
+                        <!-- <p class="mt-1" v-if="selectedSizeOption.size_description"><strong>Description:</strong> {{ selectedSizeOption.size_description }}</p> -->
+                    </div>
+
+                    <div class="flex justify-between items-center mt-5 mb-1 border font-bold text-stone-200 text-lg p-4">
+                        <span>
+                            {{ currencyFormat(product.lowest_priced_item.original_price) }}
+                        </span>
+                        <span v-if="product.lowest_priced_item && product.lowest_priced_item.sale_price < 0"
+                            class="text-red-500 font-bold text-lg">
+                            ON SAIL! {{ currencyFormat(product.lowest_priced_item.sale_price) }}
+                        </span>
+                        <span v-else class="line-through  font-bold text-gray-400">
+                            Not on sail
+                        </span>
                     </div>
                     <div class="items-center mb-1 border-b border-gray-300 ">
                         <span class="  mr-2">Description:</span>
@@ -58,47 +107,7 @@ defineProps({
                         <span class=" mr-2">About:</span>
                         <p>{{ product.about }}</p>
                     </div>
-                    <div class="flex justify-start items-center mb-1 pb-1">
-                        <div class="flex items-center mb-1 items-center mb-1 border-b border-gray-300 pb-1">
-                            <span class=" mr-2">Is Active:</span>
-                            <span :class="product.is_active ? 'text-green-500' : 'text-red-600'">{{ product.is_active ?
-                                '&#x2714;' : ' &#x2716;' }}</span>
-                        </div>
-                        <div class="flex items-center mb-1 border-b border-gray-300 pb-1 ml-5">
-                            <span class=" mr-2">Is Featured:</span>
-                            <span :class="product.is_featured ? 'text-green-100' : 'text-red-600'">{{
-                                product.is_featured ?
-                                    '&#x2714;'
-                                : ' &#x2716;' }}</span>
-                        </div>
-                    </div>
 
-                    <div>
-                        <label for="countries"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
-                            an option</label>
-                        <select id="countries"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option selected>Choose size</option>
-                            <option v-for="size_options in product.lowest_priced_item.size_options"
-                                :key="size_options.id" value="{{size_options.id}}">{{ size_options.name }}</option>
-
-                        </select>
-                    </div>
-
-                    <div
-                        class="flex justify-between items-center mt-5 mb-1 border font-bold text-stone-200 text-lg p-4">
-                        <span>
-                            {{ currencyFormat(product.lowest_priced_item.original_price) }}
-                        </span>
-                        <span v-if="product.lowest_priced_item && product.lowest_priced_item.sale_price < 0"
-                            class="text-red-500 font-bold text-lg">
-                            ON SAIL! {{ currencyFormat(product.lowest_priced_item.sale_price) }}
-                        </span>
-                        <span v-else class="line-through  font-bold text-gray-400">
-                            Not on sail
-                        </span>
-                    </div>
                 </div>
             </div>
             <h2 class="text-xl text-cyan-950 font-semibold m-2 mt-3">Product Variants</h2>
