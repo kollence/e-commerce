49a1612ea13aa78f411c441df01f2aab8e587da6
kollence <misterija@gmail.com>
2024-10-04 19:16:26 +0200

    session lifetime on 72h

--- config/session.php
+++ config/session.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 4320), // 4320 = 72 hours
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 
