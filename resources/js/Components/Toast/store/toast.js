ce0223f5740d208ea70820a12f97ced4ef47e228
kollence <misterija@gmail.com>
2024-10-08 19:02:26 +0200

    Toast components with composition data

---
+++ resources/js/Components/Toast/store/toast.js
@@ -0,0 +1,18 @@
+import { reactive } from "vue";
+
+export default reactive({
+    items: [],
+    
+    addToast(toast) {
+        this.items.unshift({
+            key: Symbol(),
+            ...toast
+        });
+    },
+    removeToast(index) {
+        this.items.splice(index, 1);
+    },
+    clearToasts() {
+        this.items = [];
+    },
+})
