ee070cde12c8ae337c2b6ffd78caa083c7017658
kollence <misterija@gmail.com>
2024-10-15 21:44:25 +0200

    Get query string & filter data by Category & sort

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -15,8 +15,10 @@ class ShopController extends Controller
      */
     public function index()
     {
+        $sort = request()->input('sort'); // Default to 'newest'
         $categories = Category::where('parent_category_id', null)->with('children')->get();
         $breadcrumbs = [];
+        $queryParams = collect();
         if (request()->category) {
             // Fetch the requested category
             $category = Category::where('slug', request()->category)->with('allParents')->firstOrFail();
@@ -26,26 +28,51 @@ class ShopController extends Controller
         
             $breadcrumbs = $this->getBreadcrumbs($category);
             $selectedCategory = $category->name;
+            $queryParams->put('category', $category->slug);
             // Get all the IDs of the requested category and its children
             $categoryIds = $category->children->pluck('id')->toArray();
             $categoryIds[] = $category->id; // Include the requested category itself
-
+    
             // Fetch products that belong to any of these categories
-            $products = Product::whereHas('categories', function ($query) use ($categoryIds) {
+            $query = Product::whereHas('categories', function ($query) use ($categoryIds) {
                 $query->whereIn('categories.id', $categoryIds);
             })
-                ->with(['productItem.images', 'productItem.color'])
-                ->get();
+            ->with(['productItem.images', 'productItem.color']);
         } else {
             $selectedCategory = 'All';
-            $products = Product::with(['productItem.images', 'productItem.color'])->get();
+            $query = Product::with(['productItem.images', 'productItem.color']);
         }
-        // dd($products);
+        $products = $query->get();
+        // Apply sorting
+        if(request()->sort){
+            $queryParams->put('sort', $sort);
+            switch ($sort) {
+                case 'is_featured':
+                    $products = $products->where('is_featured', true);
+                    break;
+                case 'price_ascending':
+                    $products = $products->sortBy('productItem.price');
+                    break;
+                case 'price_descending':
+                    $products = $products->sortByDesc('productItem.price');
+                    break;
+                case 'newest':
+                    $products = $products->sortByDesc('productItem.created_at');
+                    break;
+                case 'oldest':
+                    $products = $products->sortBy('productItem.created_at');
+                    break;
+            }
+        }
+        // dd($queryParams);
+    
         return inertia('Shop/Index', [
-            'products' => $products,
+            'products' => $products->values()->all(), // Ensure the collection is re-indexed,
             'categories' => $categories,
             'selectedCategory' => $selectedCategory,
+            'queryParams' => $queryParams, // Pass the query parameters to the view
             'breadcrumbs' => $breadcrumbs,
+            'sort' => $sort,
         ]);
     }
     
@@ -98,7 +125,7 @@ class ShopController extends Controller
         return inertia('Shop/Show', [
             'product' => $product->load('brand'),
             'productItems' => $productItems,
-            'productItem' => $productItem,       // DODAJ JOS KATEGORIJA NASTAVI ODAVDE JER SVE RADI SAMO VIDI BAGOVE KADA PRIPADA U VISE KATEGORIJA
+            'productItem' => $productItem,       // DODAJ JOS KATEGORIJA. NASTAVI ODAVDE JER SVE RADI, SAMO VIDI BAGOVE KADA IMA VISE KATEGORIJA
             'breadcrumbs' => $this->getBreadcrumbs($product->categories->first()),
         ]);
     }
