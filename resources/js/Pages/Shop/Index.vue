2f42d09e69ef8030dda11904743a5e3c61188ef1
kollence <misterija@gmail.com>
2024-10-15 21:41:20 +0200

    Select option onChange updateSort & update query string

--- resources/js/Pages/Shop/Index.vue
+++ resources/js/Pages/Shop/Index.vue
@@ -8,8 +8,19 @@ const props = defineProps({
     products: Object,
     categories: Object,
     selectedCategory: String,
+    queryParams: Object,
     breadcrumbs: Object,
 });
+const sort = ref('default');
+const category = ref(props.queryParams.category);
+
+const updateSort = () => {
+    // console.log(sort.value);
+  const params = new URLSearchParams(window.location.search);
+  category.value && params.set('category', category.value);
+  sort.value !== 'default' ? params.set('sort', sort.value) : params.delete('sort');
+  router.get(`${window.location.pathname}?${params.toString()}`, {}, { preserveState: true });
+};
 // NEED FIX!! only clear previous breadcrumbs (category history) so they don't pile up just in Cart/Index output
 onBeforeUnmount(() => {
     breadcrumbsStore.removeCrumbs()
@@ -34,6 +45,17 @@ onBeforeUnmount(() => {
         <NavCategories :categories="categories" />
 
         <div class="border-l w-4/5 mx-auto">
+            <div class="flex items-center justify-end space-x-2 pt-4 pr-6">
+                <span class="text-gray-500">Sort by:</span>
+                <select v-model="sort" name="sort" id="sort" class="bg-gray-200 rounded-lg py-2 px-7 focus:outline-none focus:bg-white" @change="updateSort">
+                    <option value="default" selected="selected">Pick option</option>
+                    <option value="is_featured">Featured</option>
+                    <option value="price_ascending">Price: Low to High</option>
+                    <option value="price_descending">Price: High to Low</option>
+                    <option value="newest">Newest</option>
+                    <option value="oldest">Oldest</option>
+                </select>
+            </div>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-2 md:gap-4 lg:gap-5 sm:p-0 md:p-4">
                 <Link :href="route('shop.show', [product.slug, product.product_item.id])" class="card rounded-lg p-2 shadow-md" v-for="(product, index) in products" :key="index">
                 <img v-if="product.product_item.images.length > 0" :src="product.product_item.images[0].url" :alt="product.name"
