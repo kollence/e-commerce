0f554e3146832ff880165e71b553741f43481a39
kollence <misterija@gmail.com>
2024-10-17 20:53:21 +0200

    Fix layout of products in list

--- resources/js/Pages/Shop/Index.vue
+++ resources/js/Pages/Shop/Index.vue
@@ -2,7 +2,7 @@
 import Breadcrumbs from '@/Components/LayoutPartials/Breadcrumbs.vue';
 import NavCategories from '@/Components/Shop/NavCategories.vue';
 import { Link, Head, router } from '@inertiajs/vue3';
-import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { onBeforeUnmount, ref } from 'vue';
 import breadcrumbsStore from '@/Components/LayoutPartials/store/breadcrumbs.js'
 const props = defineProps({
     products: Object,
@@ -56,25 +56,26 @@ onBeforeUnmount(() => {
                     <option value="oldest">Oldest</option>
                 </select>
             </div>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-2 md:gap-4 lg:gap-5 sm:p-0 md:p-4">
+            <div v-if="products.length > 0" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-2 md:gap-4 lg:gap-5 sm:p-0 md:p-4">
                 <Link :href="route('shop.show', [product.slug, product.product_item.id])" class="card rounded-lg p-2 shadow-md" v-for="(product, index) in products" :key="index">
                 <img v-if="product.product_item.images.length > 0" :src="product.product_item.images[0].url" :alt="product.name"
                     class="object-cover">
                 <img v-else :src="'/storage/images/defaults/default.jpg'" alt=""
                     class="object-cover">
-                <div class="flex justify-around bg-gray-200 py-2">
-                    <span>
+                <div class="flex flex-wrap gap-4 justify-between p-1">
+                    <span :class="{ 'line-through text-gray-500 col-span-auto': product.product_item.sale_price > 0}">
                         {{ currencyFormat(product.product_item.original_price) }}
                     </span>
-                    <span v-if="product.product_item && product.product_item.sale_price > 0" class="text-red-500 font-bold">
-                        ON SAIL! {{currencyFormat(product.product_item.sale_price)}}
-
+                    <span class="text-gray-500 col-span-auto text-center">
                     </span>
-                    <span v-else class="line-through text-gray-500">
-                        Not on sail
+                    <span v-if="product.product_item && product.product_item.sale_price > 0" class="text-right text-red-500 font-bold col-span-auto">
+                        On sail! {{currencyFormat(product.product_item.sale_price)}}
                     </span>
                 </div>
                 </Link>
+            </div>
+            <div v-else class="flex justify-center">
+                <h1 class="text-2xl font-bold text-gray-500" >Products not found.</h1>
             </div>
         </div>
     </div>
