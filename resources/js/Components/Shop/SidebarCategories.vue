3f3cbca9e89ff7cd69da751fbc883582317367d6
kollence <misterija@gmail.com>
2024-10-27 18:58:26 +0100

    SidebarCategories component with RecursiveCategories

---
+++ resources/js/Components/Shop/SidebarCategories.vue
@@ -0,0 +1,19 @@
+<script setup>
+import RecursiveCategories from './RecursiveCategories.vue';
+
+const props = defineProps({
+    categories: Object
+})
+
+</script>
+
+<template>
+    <div class="w-1/5 text-xl mx-auto shrink-0">
+        <nav>
+            <RecursiveCategories v-for="category in categories" :category="category" :key="category.id" />
+        </nav>
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
