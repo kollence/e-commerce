af38d879bae1ad20d1841ed41c6d327972d53c3f
kollence <misterija@gmail.com>
2024-11-14 19:31:21 +0100

    Revert "Change name field for Address from country to state because Stripe use `state`"
    This reverts commit ccfaea0e808a497654e60872f0dfbf2ed1700618.

--- database/migrations/2024_08_15_185410_create_addresses_table.php
+++ database/migrations/2024_08_15_185410_create_addresses_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
 
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->string('street_and_number');
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->string('street_and_number')->nullable();
             $table->string('city');
-            $table->string('state')->nullable();
+            $table->string('country');
             $table->string('zip_code');
-            $table->string('phone_1');
+            $table->string('phone_1')->nullable();
             $table->string('phone_2')->nullable();
             $table->boolean('default')->default(false);
             $table->timestamps();
