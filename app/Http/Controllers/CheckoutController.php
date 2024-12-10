1a0f6b4d2886dc095940b5b5aef8dc195414ac38
kollence <misterija@gmail.com>
2024-12-10 16:57:11 +0100

    Test Simple one time charge-(deprecated) without need to auth user-Billable

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
+use App\Models\Order;
+use App\Models\User;
 use App\Services\CartService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CheckoutController extends Controller
 {
@@ -37,7 +42,85 @@ class CheckoutController extends Controller
      */
     public function store(Request $request)
     {
-        // dd($request->all());
+        // $user = $request->user(); 
+        // // Validate the request 
+        $request->validate([ 
+            'payment_method_id' => 'required|string', 
+            'amount' => 'required|numeric|min:1', 
+            // 'shipping_address.street_and_number' => 'required|string', 
+            // 'billing_address.street_and_number' => 'nullable|string' 
+        ]);
+        $paymentMethodId = $request->payment_method_id;
+        $amount = $request->amount * 100;
+
+        // DB::beginTransaction();
+
+        try {
+            // $user = new User;
+            (new User)->charge($amount, $paymentMethodId, ['return_url' => route('checkout.success')]);
+        //     // Charge the user 
+        //     $options = ['return_url' => route('checkout.success')]; 
+        //     $user->charge($amount, $paymentMethodId, $options);
+        //     // Store or update shipping address 
+        //     $shippingAddress = Address::firstOrNew( [ 
+        //         'user_id' => $user->id, 
+        //         'street_and_number' => $request->shipping_address['street_and_number'], 
+        //     ], $request->shipping_address );
+
+        //     if (!$shippingAddress->exists || $shippingAddress->isDirty()) { 
+        //         $shippingAddress->type = 'shipping';
+        //         $shippingAddress->fill($request->shipping_address); 
+        //         $shippingAddress->save(); 
+        //     } 
+        //     // Store or update billing address if provided 
+        //     $billingAddressId = null; 
+        //     if ($request->has('billing_address')) { 
+        //         $billingAddress = Address::firstOrNew( [ 
+        //             'user_id' => $user->id, 
+        //             'type' => 'billing', 
+        //             'street_and_number' => $request->billing_address['street_and_number'], 
+        //         ], $request->billing_address );
+
+        //         if (!$billingAddress->exists || $billingAddress->isDirty()) { 
+        //             $billingAddress->type = 'billing';
+        //             $billingAddress->fill($request->billing_address); 
+        //             $billingAddress->save(); 
+        //         } 
+        //         $billingAddressId = $billingAddress->id; 
+        //     } 
+        //     // Create the order 
+        //     Order::create([ 
+        //         'user_id' => $user->id, 
+        //         'total_price' => $amount, 
+        //         'status' => 'completed', 
+        //         'payment_method' => $request->payment_method, 
+        //         'shipping_method' => $request->shipping_method, 
+        //         'shipping_price' => 666, 
+        //         'currency' => 'USD', 
+        //         'shipping_address_id' => $shippingAddress->id, 
+        //         'billing_address_id' => $billingAddressId, 
+        //     ]); 
+        //     // Commit the transaction 
+        //     DB::commit(); 
+        //     // Respond with an Inertia response 
+            return inertia('Checkout/Success', ['message' => 'Payment successful']); 
+        } catch (\Exception $e) { 
+            // Rollback the transaction 
+            // DB::rollBack(); 
+            return inertia('Checkout/Canceled', ['error' => $e->getMessage()]);
+        }
+    }
+
+    public function success(Request $request)
+    {
+        // dd('success '.$request->all());
+        return inertia('Checkout/Success');
+    }
+
+    public function canceled(Request $request)
+    {
+        // dd('canceled '.$request->all());
+        return inertia('Checkout/Canceled');
     }
 
     /**
